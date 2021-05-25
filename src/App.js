825c937890c713fb23c34c7a5372d0814c026d09
virinchi-manepalli-trysolvio <virinchi.manepalli@trysolvio.ai>
2021-05-25 15:08:08 +0530

    electron app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+const App = () => {
+  const [value, setValue] = useState("");
+  const [arr, setArray] = useState(["val"]);
 
-function App() {
+  const addtodo = () => {
+    const dummy = [...arr];
+    dummy.push(value);
+    setArray(dummy);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          marginTop: "10%",
+          marginLeft: "10%",
+          width: "100%",
+          display: "flex",
+        }}
+      >
+        <input
+          style={{ width: "20%" }}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            addtodo();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          click
+        </button>
+        <button
+          onClick={() => {
+            setArray([]);
+          }}
+        >
+          clear
+        </button>
+      </div>
+      <div style={{ marginLeft: "20%" }}>
+        {arr.map((e) => {
+          console.log(e);
+          return (
+            <>
+              <h1>{e}</h1>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
